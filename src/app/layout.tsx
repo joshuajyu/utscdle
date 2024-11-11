989dcd2869b8f456e8dcec283dbd2a69e8a5d141
Joshua Yu <jo2004yu@gmail.com>
2024-11-11 00:18:28 -0500

    Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>
+      <body className={`dark ${inter.className} antialiased`}>
         <SidebarProvider>
           <div className="flex h-full w-screen">
             {/* Sidebar */}
